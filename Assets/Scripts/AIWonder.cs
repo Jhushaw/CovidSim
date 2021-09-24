b4c3b764b3c431d5219e647dd9270d8e61da3b9b
Jacob Hushaw <Jshushaw@gmail.com>
2021-09-24 15:19:26 -0700

    Set up environment and started AIWonder Script
    setup the prefab environment that the AI will interact with eachother in. Also started the AIWonder script. Still has errors with waiting and moving somewhere new.

---
+++ Assets/Scripts/AIWonder.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWonder : MonoBehaviour
+{
+
+    public float duration;  //Maximum ammount of walking time
+    float elapsedTime = 0f; //time spent walking
+    float wait = 0f; //wait this long
+    float waitTime = 0f;    //time spent waiting
+
+    float randomX;
+    float randomZ;
+
+    bool move = true; //start moving
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomX = Random.Range(-3,3);
+        randomZ = Random.Range(-8,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (elapsedTime < duration && move)
+        {
+            transform.Translate(new Vector3(randomX,0,randomZ) * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+        } else {
+            move = false;
+            wait = Random.Range(3,5);
+            waitTime = 0f;
+        }
+
+        if (waitTime < wait && !move)
+        {
+            waitTime += Time.deltaTime;
+        } else if (!move){
+            move = true;
+            elapsedTime = 0f;
+            randomX = Random.Range(-3,3);
+            randomZ = Random.Range(-8,0);
+        }
+    }
+}
