4ddf7661f8965789fac64d86d8f7544936a660ac
Jacob Hushaw <Jshushaw@gmail.com>
2021-10-05 13:29:35 -0700

    Finished agent wonder scripts

--- Assets/Scripts/AIWonder.cs
+++ Assets/Scripts/AIWonder.cs
@@ -1,47 +1,105 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AIWonder : MonoBehaviour
 {
 
-    public float duration;  //Maximum ammount of walking time
-    float elapsedTime = 0f; //time spent walking
-    float wait = 0f; //wait this long
-    float waitTime = 0f;    //time spent waiting
+    public float movespeed = 0f;
+    private Rigidbody rbody;
+    public Vector3 moveDir;
+    public LayerMask wall;
+    public float maxDistFromWall = 0f;
+    private Vector3 startpos;
+    private bool wait;
+    private float tillWait = 0f;
+    private float StopWait = 0f;
 
-    float randomX;
-    float randomZ;
+    [SerializeField] private Material covidMat;
+    [SerializeField] private MeshRenderer agentMesh;
 
-    bool move = true; //start moving
     // Start is called before the first frame update
     void Start()
     {
-        randomX = Random.Range(-3,3);
-        randomZ = Random.Range(-8,0);
+        wait = false;
+        startpos = transform.position;
+        rbody = GetComponent<Rigidbody>();
+        moveDir = ChooseDirection();
+        transform.rotation = Quaternion.LookRotation(moveDir);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (elapsedTime < duration && move)
-        {
-            transform.Translate(new Vector3(randomX,0,randomZ) * Time.deltaTime);
-            elapsedTime += Time.deltaTime;
-        } else {
-            move = false;
-            wait = Random.Range(3,5);
-            waitTime = 0f;
+        rbody.velocity = moveDir * movespeed;
+        tillWait += Time.deltaTime;
+        if (tillWait >= Random.Range(10,25)){
+            wait = true;
+            rbody.velocity = Vector3.zero;
+        }
+        if (wait == true){
+            StopWait += Time.deltaTime;
         }
 
-        if (waitTime < wait && !move)
+        if (StopWait >= Random.Range(5,20)){
+            wait = false;
+            tillWait = 0f;
+            StopWait = 0f;
+            moveDir = ChooseDirection();
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+
+        if (Physics.Raycast(transform.position, transform.forward, maxDistFromWall, wall))
         {
-            waitTime += Time.deltaTime;
-        } else if (!move){
-            move = true;
-            elapsedTime = 0f;
-            randomX = Random.Range(-3,3);
-            randomZ = Random.Range(-8,0);
+            moveDir = ChooseDirection();
+            transform.rotation = Quaternion.LookRotation(moveDir);
         }
     }
+
+    Vector3 ChooseDirection(){
+        int i = Random.Range(0,7);
+        Vector3 temp = new Vector3(0,0,0);
+
+        if(i==0){
+            temp = transform.forward;
+        } 
+        else if(i==1){
+            temp = transform.right;
+        }
+        else if(i==2){
+             temp = -transform.right;
+        }
+        else if(i==3){
+            temp = -transform.forward;
+        }
+        else if(i==4){
+            temp = new Vector3(.5f,0,.5f);
+        }
+        else if(i==5){
+            temp = new Vector3(-.5f,0,.5f);
+        }
+        else if(i==6){
+            temp = new Vector3(.5f,0,-.5f);
+        }
+        else if(i==7){
+            temp = new Vector3(-.5f,0,-.5f);
+        }
+
+        return temp;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Wall>(out Wall wall)){
+            transform.position = startpos;
+        }
+
+        if (other.TryGetComponent<CovidTrigger>(out CovidTrigger covidTrigger)){
+            
+            int randomChance = Random.Range(1,10);
+            if (randomChance == 3)
+                agentMesh.material = covidMat;
+        }
+    }
+
 }
---
+++ Assets/Scripts/CovidTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CovidTrigger : MonoBehaviour
+{
+    // private CovidTrigger ct = gameObject.GetComponent(typeof(CovidTrigger)) as CovidTrigger;
+    // [SerializeField] private Material covidMat;
+    // [SerializeField] private MeshRenderer agentMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ct.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        // if (other.TryGetComponent<CovidTrigger>(out CovidTrigger covidTrigger)){
+            
+        //     int randomChance = Random.Range(1,10);
+        //     if (randomChance == 3)
+        //         agentMesh.material = covidMat;
+        //         ct.enabled = true;
+        // }
+    }
+}
